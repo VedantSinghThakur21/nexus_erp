45466bec195ea0ec1b18a5e730c210e47a8fb8ad
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2026-03-03 21:52:51 +0530

    routing error for sales order page

---
+++ app/tenant/(main)/sales-orders/[id]/page.tsx
@@ -0,0 +1,6 @@
+export const dynamic = 'force-dynamic'
+
+/**
+ * Tenant sales order detail page (tesla.avariq.in/sales-orders/[id])
+ */
+export { default } from '@/app/(main)/sales-orders/[id]/page'
---
+++ app/tenant/(main)/sales-orders/new/page.tsx
@@ -0,0 +1,6 @@
+export const dynamic = 'force-dynamic'
+
+/**
+ * Tenant new sales order page (tesla.avariq.in/sales-orders/new)
+ */
+export { default } from '@/app/(main)/sales-orders/new/page'
