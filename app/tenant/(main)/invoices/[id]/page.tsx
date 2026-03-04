878a43419a779764fc6fa5e7e7db72e6e07c635d
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2026-03-04 22:36:27 +0530

    fixing page after save invoice

---
+++ app/tenant/(main)/invoices/[id]/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic'
+
+/**
+ * Tenant invoice detail page (vfixit.avariq.in/invoices/[id])
+ * Re-exports the main invoice detail page.
+ */
+export { default } from '@/app/(main)/invoices/[id]/page'
