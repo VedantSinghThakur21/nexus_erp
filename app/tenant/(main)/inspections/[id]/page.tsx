8ea4aea0d87e0e4d6b2f3d9131eaa5a98b2f09b5
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2026-03-04 23:08:34 +0530

    fillin gin the missing imports

---
+++ app/tenant/(main)/inspections/[id]/page.tsx
@@ -0,0 +1,2 @@
+export const dynamic = 'force-dynamic'
+export { default } from '@/app/(main)/inspections/[id]/page'
---
+++ app/tenant/(main)/payments/[id]/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic'
+
+/**
+ * Tenant payment detail page (vfixit.avariq.in/payments/[id])
+ * Re-exports the main payment detail page.
+ */
+export { default } from '@/app/(main)/payments/[id]/page'
